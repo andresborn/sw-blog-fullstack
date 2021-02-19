29fc23f2b89ddc6fa4a34e4feced56e46c7068e5
andresborn <andres.born@gmail.com>
2021-02-18 22:48:50 -0600

    Conditionally render obiwan to avoid crash

--- src/views/PersonProfile.js
+++ src/views/PersonProfile.js
@@ -13,13 +13,16 @@ const PersonProfile = ({ match }) => {
   return (
     <div className="container">
       <h1 className="breadcrumb">{person.name}</h1>
-      <h6 className="breadcrumb bg-warning">Homeworld: {homeworld.name}</h6>
-
-      <div className="row d-flex flex-row">
-      <div className="col-8">
+      { // Obi-Wan is missing planet, conditionally render out his homeworld
+        person.name === "Obi-Wan Kenobi" 
+        ? null 
+        : <h6 className="breadcrumb bg-warning">Homeworld: {homeworld.name}</h6>
+        }
+      <div className="row d-flex flex-column">
+      <div className="col-12">
       <img className="pb-5" style={{maxWidth:"100%", objectFit: "cover", objectPosition: "top"}} src={`${actions.getPersonImage(person)}`} alt=""/>
       </div>
-      <div className="col-4">
+      <div className="col-12">
         
           <ul className="list-group" style={{ listStyle: "none" }}>
             <li className="list-group-item">Birth year: {person.birth_year}</li>
